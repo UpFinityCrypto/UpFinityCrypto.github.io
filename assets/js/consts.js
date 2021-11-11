f5e640a1929290cf4b8e656add11dd4c5c1bf86a
cointracker <86149297+AllCoinLab@users.noreply.github.com>
2021-11-12 03:08:56 +0900

    Update consts.js

--- assets/js/consts.js
+++ assets/js/consts.js
@@ -5,7 +5,7 @@ const routerAdr = '0x10ED43C718714eb63d5aA57B78B54704E256024E';
 const refLinkPrefix = 'https://upfinitycrypto.github.io?ref=';
 const upfinityAdr = '0x6CC5F09E46797189D18Ea8cfb3B1AaA4661280Ae';
 
-const inputHandlerBuy = function(e) {
+inputHandlerBuy = function(e) {
     valueIn = e.target.value;
     valueIn = valueIn.replace(/,/g,'');
     result = getElement('swapOutput');
@@ -20,7 +20,7 @@ const inputHandlerBuy = function(e) {
     valueOut = parseInt(valueOut);
     result.value = numberWithCommas(valueOut);
 }
-const inputHandlerSell = function(e) {
+inputHandlerSell = function(e) {
     valueIn = e.target.value;
     valueIn = valueIn.replace(/,/g,'');
     result = getElement('swapOutput');
