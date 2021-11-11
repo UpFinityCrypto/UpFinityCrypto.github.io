ba73aa948020fd441fc4bbd3a0a8dc5353ef2985
allcoinlab <allcoinlab@gmail.com>
2021-11-11 04:37:40 +0000

    aa

--- assets/js/consts.js
+++ assets/js/consts.js
@@ -72,5 +72,6 @@ const decimals = 18;
   {"inputs": [{"internalType": "address", "name": "", "type": "address"}, {"internalType": "uint256", "name": "", "type": "uint256"}], "name": "_ownedItemCount", "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}], "stateMutability": "view", "type": "function"},
   {"inputs": [{"internalType": "address", "name": "", "type": "address"}], "name": "_ownedTotalItemCount", "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}], "stateMutability": "view", "type": "function"},
   {"inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}], "name": "_totalItemCount", "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}], "stateMutability": "view", "type": "function"},
-  
+    { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+  {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
   ];
