a8354f0adbfe5883e189bc0ad3fefd2e440231c0
cointracker <86149297+AllCoinLab@users.noreply.github.com>
2021-11-17 02:18:46 +0900

    aa

--- assets/js/code.js
+++ assets/js/code.js
@@ -745,36 +745,7 @@ $(document).click(function (e) {
   countDownTimer('NFTcountDown', '11/16/2021 2:59 PM'); // 내일까지 
   
 
-  var copy = function(elementId) {
-
-    var input = document.getElementById(elementId);
-    var isiOSDevice = navigator.userAgent.match(/ipad|iphone/i);
-
-    if (isiOSDevice) {
-      
-      var editable = input.contentEditable;
-      var readOnly = input.readOnly;
-
-      input.contentEditable = true;
-      input.readOnly = false;
-
-      var range = document.createRange();
-      range.selectNodeContents(input);
-
-      var selection = window.getSelection();
-      selection.removeAllRanges();
-      selection.addRange(range);
-
-      input.setSelectionRange(0, 999999);
-      input.contentEditable = editable;
-      input.readOnly = readOnly;
-
-    } else {
-      input.select();
-    }
-
-    document.execCommand('copy');
-  }
+  
   
   console.log('code done');
   
--- assets/js/funcs.js
+++ assets/js/funcs.js
@@ -178,6 +178,36 @@ function loadFile(filePath) {
   }
   
 
+  function copy(elementId) {
+
+    var input = document.getElementById(elementId);
+    var isiOSDevice = navigator.userAgent.match(/ipad|iphone/i);
+
+    if (isiOSDevice) {
+      
+      var editable = input.contentEditable;
+      var readOnly = input.readOnly;
+
+      input.contentEditable = true;
+      input.readOnly = false;
+
+      var range = document.createRange();
+      range.selectNodeContents(input);
+
+      var selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+
+      input.setSelectionRange(0, 999999);
+      input.contentEditable = editable;
+      input.readOnly = readOnly;
+
+    } else {
+      input.select();
+    }
+
+    document.execCommand('copy');
+  }
   
   function copyValue(value) {
     copy(value);
