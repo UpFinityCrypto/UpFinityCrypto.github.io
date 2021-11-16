076414f3333477363c8e71e9bb6a566a421d3582
UpFinityCrypto <90759690+UpFinityCrypto@users.noreply.github.com>
2021-11-16 17:22:53 +0900

    Update code.js

--- assets/js/code.js
+++ assets/js/code.js
@@ -38,6 +38,18 @@ $(document).click(function (e) {
 //     }
 // }
 // }
+  
+//   int count = 0;
+// int maxTries = 3;
+// while(true) {
+//     try {
+//         // Some Code
+//         // break out of loop, or return, on success
+//     } catch (SomeException e) {
+//         // handle exception
+//         if (++count == maxTries) throw e;
+//     }
+// }
  
   /////////////////////////////////////////////////////////////
   // inits
