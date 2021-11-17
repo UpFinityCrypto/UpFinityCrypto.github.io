6d58cd86cf791871938349128726e05430d1e9c7
UpFinityCrypto <90759690+UpFinityCrypto@users.noreply.github.com>
2021-11-17 15:17:30 +0900

    Update consts.js

--- assets/js/consts.js
+++ assets/js/consts.js
@@ -4,6 +4,7 @@ const wbnbAdr = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
 const routerAdr = '0x10ED43C718714eb63d5aA57B78B54704E256024E';
 const refLinkPrefix = 'https://upfinitycrypto.github.io?ref=';
 const upfinityAdr = '0x6CC5F09E46797189D18Ea8cfb3B1AaA4661280Ae';
+const burnAdr = "0x000000000000000000000000000000000000dEaD";
 
 buyFee = 900;
 displayText("buyFee", buyFee / 100);
