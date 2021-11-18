f8347c1a4e8fadd0286df80088b60232f3344364
cointracker <86149297+AllCoinLab@users.noreply.github.com>
2021-11-18 19:05:52 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,6 +92,7 @@
    * Mobile nav toggle
    */
   on('click', '.mobile-nav-toggle', function(e) {
+    console.log('done');
     select('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
