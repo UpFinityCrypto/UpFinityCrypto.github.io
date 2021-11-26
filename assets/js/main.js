70fc1503b2e43b2478cb4084287553129c1a11c6
cointracker <86149297+AllCoinLab@users.noreply.github.com>
2021-11-26 23:21:59 +0900

    Revert "Update main.js"
    This reverts commit ebd2afc23df526bc666b1aa667ec2d05d08e0092.

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,20 +111,20 @@ function init() { // to use async things
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  // on('click', '.scrollto', function(e) {
-    // if (select(this.hash)) {
-      // e.preventDefault()
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
 
-      // let body = select('body')
-      // if (body.classList.contains('mobile-nav-active')) {
-        // body.classList.remove('mobile-nav-active')
-        // let navbarToggle = select('.mobile-nav-toggle')
-        // navbarToggle.classList.toggle('bi-list')
-        // navbarToggle.classList.toggle('bi-x')
-      // }
-      // scrollto(this.hash)
-    // }
-  // }, true)
+      let body = select('body')
+      if (body.classList.contains('mobile-nav-active')) {
+        body.classList.remove('mobile-nav-active')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
 
   /**
    * Scroll with ofset on page load with hash links in the url
