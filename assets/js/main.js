c5f6e51a8cc5beeff9567ec40079192a2e12a5a7
UpFinityCrypto <90759690+UpFinityCrypto@users.noreply.github.com>
2021-11-26 09:20:13 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,6 +97,16 @@
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
+  
+  /**
+  * Mobile nav dropdowns activate
+  */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
 
   /**
    * Scrool with ofset on links with a class name .scrollto
