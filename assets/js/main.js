4a5a48f89299d9bfe820b89375d9b24be773caee
UpFinityCrypto <90759690+UpFinityCrypto@users.noreply.github.com>
2021-11-26 09:50:58 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,6 +99,15 @@
   })
   
   /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+  
+  /**
   * Mobile nav dropdowns activate
   */
   on('click', '.navbar .dropdown > a', function(e) {
