2d7636ee50bae788d4ac6ff6007374e566eefa1f
cointracker <86149297+AllCoinLab@users.noreply.github.com>
2021-12-01 12:42:01 +0900

    Update consts.js

--- assets/js/consts.js
+++ assets/js/consts.js
@@ -58,9 +58,6 @@ inputHandlerSell = function (e) {
 inputHandlerStake = function (e) {
   (async function () {
     typedStakeAmount = getElement('typedStakeAmount');
-    stake1d = getElement('stake1d');
-    stake7d = getElement('stake7d');
-    stake28d = getElement('stake28d');
 
     valueIn = e.target.value;
     valueIn = valueIn.replace(/,/g, '');
@@ -68,9 +65,9 @@ inputHandlerStake = function (e) {
     if (valueIn == 0) {
       typedStakeAmount.value = valueIn;
 
-      stake1d.value = '1 days: ' + String(valueIn) + ' Reward';
-      stake7d.value = '7 days: ' + String(valueIn) + ' Reward';
-      stake28d.value = '28 days: ' + String(valueIn) + ' Reward';
+      displayText('stake1d', '1 days (Reward: ' + String(valueIn) + ')');
+      displayText('stake7d', '7 days (Reward: ' + String(valueIn) + ')');
+      displayText('stake28d', '28 days (Reward: ' + String(valueIn) + ')');
       return;
     }
 
@@ -78,9 +75,9 @@ inputHandlerStake = function (e) {
     valueInComma = numberWithCommas(valueIn);
     typedStakeAmount.value = valueInComma;
 
-    stake1d.value = '1 days: ' + String(abbreviateNumber(valueIn * 10019 / 10000000)) + ' Reward';
-    stake7d.value = '7 days: ' + String(abbreviateNumber(valueIn * 123208 / 10000000)) + ' Reward';
-    stake28d.value = '28 days: ' + String(abbreviateNumber(valueIn * 828819 / 10000000)) + ' Reward';
+    displayText('stake1d', '1 days (Reward: ' + String(abbreviateNumber(valueIn * 10019 / 10000000)) + ')');
+    displayText('stake7d', '7 days (Reward: ' + String(abbreviateNumber(valueIn * 123208 / 10000000)) + ')');
+    displayText('stake28d', '28 days (Reward: ' + String(abbreviateNumber(valueIn * 828819 / 10000000)) + ')');
   })();
 }
 
