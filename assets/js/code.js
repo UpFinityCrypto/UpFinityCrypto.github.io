e69d4bef4e7b0c2f0d865980f6d2cba7cad1f511
cointracker <86149297+AllCoinLab@users.noreply.github.com>
2021-12-08 03:14:41 +0900

    aa

--- assets/js/code.js
+++ assets/js/code.js
@@ -611,8 +611,8 @@ $(document).click(function (e) {
         output = `
           <div class="col-12 col-lg-3 text-justify content">
             <div style="width: 100%; position: relative;">
-              <a href="assets/img/nft/origins/${myNFTimgSrc}">
-              <img src="assets/img/nft/origins/${myNFTimgThumbSrc}" style="top:0; left: 0; padding: 20px; height: auto;">
+              <a href="assets/img/new/${myNFTimgSrc}">
+              <img src="assets/img/new/origins/${myNFTimgThumbSrc}" style="top:0; left: 0; padding: 20px; height: auto;">
               <img src="assets/img/nft/origins/${myNFTborder}_thumb.png" style="position: absolute; top:0; left: 0;">
               </a>
             </div>
