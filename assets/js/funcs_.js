1d2fac3801c5b86c0ca34c9549738f396e2eead0
cointracker <86149297+AllCoinLab@users.noreply.github.com>
2021-12-18 00:21:59 +0900

    a

--- assets/js/funcs_.js
+++ assets/js/funcs_.js
@@ -37,44 +37,6 @@ runs['Status'] = async function runStatus() {
   //       displayText("sellCooltime", d);
   //     }
 
-
-
-  displayText('priceCounter', (price / 1).toFixed(10));
-  displayText('burnCounter', abbreviateNumber(parseInt(burnAmount / bnbDiv)));
-  displayText('circulateCounter', abbreviateNumber(parseInt(realSupply / bnbDiv)));
-  displayText('marketcapCounter', numberWithCommas(mcap.toFixed(0)));
-  displayText('manualBurnCounter', burnPercentage);
-  displayText('manualLPBurnCounter', burnLpPercentage);
-  displayText('startMultiCounter', (mcap / 333).toFixed(0));
-
-  //var elms_ = document.querySelectorAll("[id='priceCounter']");
-  //if (elms_.length) {
-  //elms_[0].setAttribute('data-purecounter-end', (price / 1).toFixed(10));
-  //}
-  //var elms_ = document.querySelectorAll("[id='burnCounter']");
-  //if (elms_.length) {
-  //elms_[0].setAttribute('data-purecounter-end', burnAmount / bnbDiv);
-  //}
-  //var elms_ = document.querySelectorAll("[id='circulateCounter']");
-  //if (elms_.length) {
-  //elms_[0].setAttribute('data-purecounter-end', realSupply / bnbDiv);
-  //}
-  //var elms_ = document.querySelectorAll("[id='marketcapCounter']");
-  //if (elms_.length) {
-  //elms_[0].setAttribute('data-purecounter-end', mcap.toFixed(0));
-  //}
-  //var elms_ = document.querySelectorAll("[id='manualBurnCounter']");
-  //if (elms_.length) {
-  //elms_[0].setAttribute('data-purecounter-end', burnPercentage);
-  //}
-  //var elms_ = document.querySelectorAll("[id='manualLPBurnCounter']");
-  //if (elms_.length) {
-  //elms_[0].setAttribute('data-purecounter-end', burnLpPercentage);
-  //}
-  //var elms_ = document.querySelectorAll("[id='startMultiCounter']");
-  //if (elms_.length) {
-  //elms_[0].setAttribute('data-purecounter-end', mcap.toFixed(0) / 333);
-  //}
 }
 
 
@@ -197,6 +159,48 @@ runPersonals['Status'] = async function runStatusPersonal() {
   // price change accumulation => global limit
   // x / r0 = a => price change = 1 / (1 + a)^2
 }
+
+
+runs['Stats'] = async function runStats() {
+  displayText('priceCounter', (price / 1).toFixed(10));
+  displayText('burnCounter', abbreviateNumber(parseInt(burnAmount / bnbDiv)));
+  displayText('circulateCounter', abbreviateNumber(parseInt(realSupply / bnbDiv)));
+  displayText('marketcapCounter', numberWithCommas(mcap.toFixed(0)));
+  displayText('manualBurnCounter', burnPercentage);
+  displayText('manualLPBurnCounter', burnLpPercentage);
+  displayText('startMultiCounter', (mcap / 333).toFixed(0));
+
+    //var elms_ = document.querySelectorAll("[id='priceCounter']");
+    //if (elms_.length) {
+    //elms_[0].setAttribute('data-purecounter-end', (price / 1).toFixed(10));
+    //}
+    //var elms_ = document.querySelectorAll("[id='burnCounter']");
+    //if (elms_.length) {
+    //elms_[0].setAttribute('data-purecounter-end', burnAmount / bnbDiv);
+    //}
+    //var elms_ = document.querySelectorAll("[id='circulateCounter']");
+    //if (elms_.length) {
+    //elms_[0].setAttribute('data-purecounter-end', realSupply / bnbDiv);
+    //}
+    //var elms_ = document.querySelectorAll("[id='marketcapCounter']");
+    //if (elms_.length) {
+    //elms_[0].setAttribute('data-purecounter-end', mcap.toFixed(0));
+    //}
+    //var elms_ = document.querySelectorAll("[id='manualBurnCounter']");
+    //if (elms_.length) {
+    //elms_[0].setAttribute('data-purecounter-end', burnPercentage);
+    //}
+    //var elms_ = document.querySelectorAll("[id='manualLPBurnCounter']");
+    //if (elms_.length) {
+    //elms_[0].setAttribute('data-purecounter-end', burnLpPercentage);
+    //}
+    //var elms_ = document.querySelectorAll("[id='startMultiCounter']");
+    //if (elms_.length) {
+    //elms_[0].setAttribute('data-purecounter-end', mcap.toFixed(0) / 333);
+    //}
+}
+
+runPersonals['Stats'] = async function runStatsPersonal() {}
 
 runs['Nft'] = async function runNft() {
   id2Names = {
