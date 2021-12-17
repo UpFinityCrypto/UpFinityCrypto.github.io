174e4cacb44164cf134efd64b62e449fcb154a16
UpFinityCrypto <90759690+UpFinityCrypto@users.noreply.github.com>
2021-12-17 13:09:08 +0900

    Update funcs_.js

--- assets/js/funcs_.js
+++ assets/js/funcs_.js
@@ -719,8 +719,6 @@ async function runCode() {
   t = TT('others done', t);
   displayText('debug', 'others done');
 
-  displayText("connectResult", currentAccount + " <span>Done!</span>");
-
   // displayText('<iframe data-aa="1829702" src="//ad.a-ads.com/1829702?size=120x60" style="width:120px; height:60px; border:0px; padding:0; overflow:hidden; background-color: transparent;" ></iframe>');
 
   // if staked, run staked();
