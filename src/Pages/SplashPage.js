d0e67dd564ba5df2ed1158ea5b66ad94105243f0
upfinitycrypto <all.coin.lab@gmail.com>
2022-02-09 15:23:41 +0900

    a

--- src/Pages/SplashPage.js
+++ src/Pages/SplashPage.js
@@ -13,6 +13,8 @@ function SwapPage() {
         <h1>UPFINITY</h1>
         <p>Experience a stay in a world wide ecosystem where every feature and utility works in your favor.</p>
         <p>JOIN US, TREAT YOURSELF</p>
+        <p>Relaunch! UPF Holders will be auto migrated!</p>
+        <p>Check <a href="https://t.me/TheWeb3Project">Telegram</a> and <a href="https://www.theweb3project.com">new website!</a></p>
         <div className="redirect">
           <h2 className="button"><Link to="/main">LET ME IN </Link></h2>
           <h2 className="button"><Link to="/ecosystem">I'D LIKE TO LEARN MORE</Link></h2>
